6d23d8d33efff19bfeceb1ebdbe19af16df43c11
Aniket Shinde <anikets@criscoconsulting.in>
2021-11-19 12:20:21 +0530

    added color to report

--- ess/employee_self_service_portal/report/total_working_hours/total_working_hours.js
+++ ess/employee_self_service_portal/report/total_working_hours/total_working_hours.js
@@ -10,5 +10,29 @@ frappe.query_reports["Total Working Hours"] = {
 			fieldtype: "Link",
 			options: "Employee"
 		}
-	]
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (data.overtime >0) {
+			value = $(`<span>${value}</span>`);
+				var $value = $(value).css("font-weight", "bold");
+                $value.addClass("text-success");
+				if (data[column.fieldname] > 0) {
+				}
+
+				value = $value.wrap("<p></p>").parent().html();
+
+		}
+        else if (data.overtime <0) {
+			value = $(`<span>${value}</span>`);
+				var $value = $(value).css("font-weight", "bold");
+                $value.addClass("text-danger");
+				if (data[column.fieldname] > 0) {
+				}
+
+				value = $value.wrap("<p></p>").parent().html();
+
+		}
+		return value;
+	}
 };
